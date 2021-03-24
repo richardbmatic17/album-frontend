463361ed55f9266b262682bcacb4bf6dd50fee9b
Richard Matic <richardbmatic17@gmail.com>
2021-03-24 23:25:57 +0800

    update parallax component

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   bgParallax: {
-    position: 'relative',
-    opacity: 0.65,
-    'background-attachment': 'fixed',
-    'background-position': 'center',
-    'background-repeat': 'no-repeat',
-    'background-size': 'cover',
-    'background-image': props => `url(${props.parallaxImage})`,
-    "min-height": '100vh',
+    backgroundAttachment: 'fixed',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundImage: props => `url(${props.parallaxImage})`,
+    minHeight: '100vh',
+    display: 'flex',
   },
+  span: {
+    padding: '20px',
+    backgroundColor: 'black',
+    opacity: '.5',
+    color: 'white',
+    fontSize: 'x-large',
+    fontWeight: '900',
+  }
 }));
 
 export default function ContainedButtons(props) {
@@ -19,7 +29,18 @@ export default function ContainedButtons(props) {
 
   return (
     <div className={classes.bgParallax}>
-      asdasdasdas
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Link href={props.link}>
+          <span className={classes.span}>
+            {props.parallaxMessage}
+          </span>
+        </Link>
+      </Grid>
     </div>
   );
 }
