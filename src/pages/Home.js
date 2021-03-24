c1b1cf9f48e605c4414d0e93b690fccdae97f5fa
Richard Matic <richardbmatic17@gmail.com>
2021-03-24 23:26:12 +0800

    partial commit for homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,16 @@ const Home = (props) => {
   return (
     <div>
       <Drawer />
-      <Parallax parallaxImage="https://album-2021-03-02.s3-ap-southeast-1.amazonaws.com/boracay/IMG20200217165328.jpg" />
+      <Parallax
+        parallaxImage="https://album-2021-03-02.s3-ap-southeast-1.amazonaws.com/boracay/IMG20200217165328.jpg"
+        parallaxMessage="Welcome to Our Blog!"
+      />
       <div>Welcome to home page!</div>
+      <Parallax
+        parallaxImage="https://album-2021-03-02.s3-ap-southeast-1.amazonaws.com/boracay/IMG20200217165328.jpg"
+        parallaxMessage="Boracay Adventure"
+        link="/portfolio"
+      />
     </div>
   )
 }
