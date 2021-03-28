1647dd47c01086e2746a24b84737ec3233d84dc8
Richard Matic <richardbmatic17@gmail.com>
2021-03-28 17:48:20 +0800

    partial commit for album

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,11 +1,77 @@
 import React from 'react'
 import Drawer from '../components/Drawer'
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import Parallax from '../components/Parallax'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: '900px',
+    height: '600px',
+  },
+}));
+
+const tileData = [
+  {
+     img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
+     title: 'Image',
+     author: 'author',
+     cols: 1,
+  },
+  {
+     img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
+     title: 'Image',
+     author: 'author',
+     cols: 2,
+  },
+  {
+     img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
+     title: 'Image',
+     author: 'author',
+     cols: 2,
+  },
+  {
+     img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
+     title: 'Image',
+     author: 'author',
+     cols: 1,
+  },
+  {
+     img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
+     title: 'Image',
+     author: 'author',
+     cols: 3,
+  },
+];
 
 const Album = (props) => {
+  const classes = useStyles();
+
   return (
     <div>
       <Drawer />
-      <div>This is my album page</div>
+      <Parallax
+        parallaxImage="https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/"
+        parallaxMessage="Welcome to Album Lists"
+      />
+    <br />
+      <div className={classes.root}>
+        <GridList cellHeight={200} className={classes.gridList} cols={3}>
+          {tileData.map((tile) => (
+            <GridListTile key={tile.img} cols={tile.cols || 1}>
+              <img src={tile.img} alt={tile.title} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
     </div>
   )
 }
