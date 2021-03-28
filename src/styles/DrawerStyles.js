fa32837acb7eba18be5eb41d33c9447c10a4ca98
Richard Matic <richardbmatic17@gmail.com>
2021-03-29 01:19:47 +0800

    Fix drawer position

--- src/styles/DrawerStyles.js
+++ src/styles/DrawerStyles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
   root: {
-    position: 'absolute',
+    position: 'fixed',
     margin: '10px',
   },
   logo: {
