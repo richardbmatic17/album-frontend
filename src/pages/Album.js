10062a87a98f7567f1baced9a0f3db4523ed6718
Richard Matic <richardbmatic17@gmail.com>
2021-03-30 23:31:09 +0800

    add static data in album

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -20,46 +20,52 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const tileData = [
+const data = [
   {
-     img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
-     title: 'Image',
-     author: 'author',
-     cols: 1,
+    id: 1,
+    img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
+    title: 'Boom Paness!!!',
+    author: 'Richard Matic',
+    cols: 1,
   },
   {
-     img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
-     title: 'Image',
-     author: 'author',
-     cols: 2,
+    id: 2,
+    img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
   },
   {
-     img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
-     title: 'Image',
-     author: 'author',
-     cols: 2,
+    id: 3,
+    img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
+    title: 'Image',
+    author: 'author',
+    cols: 2,
   },
   {
-     img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
-     title: 'Image',
-     author: 'author',
-     cols: 1,
+    id: 4,
+    img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
+    title: 'Image',
+    author: 'author',
+    cols: 1,
   },
   {
-     img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
-     title: 'Image',
-     author: 'author',
-     cols: 3,
+    id: 5,
+    img: 'https://via.placeholder.com/400.png/09f/fff%20C/O%20https://placeholder.com/',
+    title: 'Image',
+    author: 'author',
+    cols: 3,
   },
 ];
 
 const Album = (props) => {
   const classes = useStyles();
   let { albumId } = useParams();
+  const pageData = data.filter(item => item.id.toString() === albumId ? item : '');
 
   return (
     <Page>
-      this is album {albumId}
+      {JSON.stringify(pageData[0])}
     </Page>
   )
 }
