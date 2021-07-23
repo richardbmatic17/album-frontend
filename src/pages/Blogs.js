8312809b6d9a41e032ac1649d49e1cc0be1bf698
Richard Matic <richardbmatic17@gmail.com>
2021-07-23 18:06:31 +0800

    initial commit for profile section

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
   Grid,
-  Paper
+  Paper,
+  Typography
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Page } from '../partials'
@@ -30,26 +37,53 @@ const Home = (props) => {
     <Page>
       <Grid container spacing={3} className={classes.root}>
         <Grid item md={3} xs={12}>
-          <Paper className={classes.paper}>
-            Profile section
+          <Paper elevation={3} className={classes.paper}>
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  height="140"
+                  image="https://album-2021-03-02.s3-ap-southeast-1.amazonaws.com/boracay/IMG_0623.JPEG"
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                  Profile section
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                    across all continents except Antarctica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  Share
+                </Button>
+                <Button size="small" color="primary">
+                  Learn More
+                </Button>
+              </CardActions>
+            </Card>
           </Paper>
-          <Paper className={classes.paper}>
+          <Paper elevation={3} className={classes.paper}>
             Recent activity section
           </Paper>
         </Grid>
         <Grid item md={6} xs={12}>
-          <Paper className={classes.paper}>Share Box</Paper>
-          <Paper className={classes.paper}>Post Section</Paper>
-          <Paper className={classes.paper}>Post 1</Paper>
-          <Paper className={classes.paper}>Post 2</Paper>
-          <Paper className={classes.paper}>Post 3</Paper>
-          <Paper className={classes.paper}>Post 4</Paper>
-          <Paper className={classes.paper}>Post 5</Paper>
-          <Paper className={classes.paper}>Post 6</Paper>
+          <Paper elevation={3} className={classes.paper}>Share Box</Paper>
+          <Paper elevation={3} className={classes.paper}>Post Section</Paper>
+          <Paper elevation={3} className={classes.paper}>Post 1</Paper>
+          <Paper elevation={3} className={classes.paper}>Post 2</Paper>
+          <Paper elevation={3} className={classes.paper}>Post 3</Paper>
+          <Paper elevation={3} className={classes.paper}>Post 4</Paper>
+          <Paper elevation={3} className={classes.paper}>Post 5</Paper>
+          <Paper elevation={3} className={classes.paper}>Post 6</Paper>
         </Grid>
         <Grid item md={3} xs={12}>
-          <Paper className={classes.paper}>Featured seciont</Paper>
-          <Paper className={classes.paper}>Ads</Paper>
+          <Paper elevation={3} className={classes.paper}>Featured seciont</Paper>
+          <Paper elevation={3} className={classes.paper}>Ads</Paper>
         </Grid>
       </Grid> 
     </Page>
