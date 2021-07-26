0daa77b18ab0ab80deb6d30c04d1b80c59d97417
Richard Matic <richardbmatic17@gmail.com>
2021-07-26 22:32:50 +0800

    added link

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -11,6 +11,7 @@ import {
   IconButton,
   Paper,
   Typography,
+  Link,
 } from '@material-ui/core';
 import {
   Favorite,
@@ -61,26 +62,33 @@ const useStyles = makeStyles((theme) => ({
   },
   cardActions: {
     padding: '0',
+  },
+  link: {
+    textDecoration: 'none !important',
   }
 }));
 
 const posts = [
   {
+    id: '1',
     title: 'Boracay Adventure',
     description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
     image: 'https://album-2021-03-02.s3-ap-southeast-1.amazonaws.com/boracay/IMG_0623.JPEG'
   },
   {
+    id: '2',
     title: 'Random Post 1',
     description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
     image: 'https://source.unsplash.com/random'
   },
   {
+    id: '3',
     title: 'Random Post 2',
     description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
     image: 'https://source.unsplash.com/random'
   },
   {
+    id: '4',
     title: 'Random Post 3',
     description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
     image: 'https://source.unsplash.com/random'
@@ -134,21 +142,23 @@ const Home = (props) => {
           {posts.map(post => (
             <Paper elevation={3} className={classes.paper} key={post.title}>
               <Card className={classes.root}>
-                <CardActionArea>
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {post.title}
-                    </Typography>
-                    <CardMedia
-                      className={classes.media}
-                      image={post.image}
-                      title={post.title}
-                    />
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      {post.description}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
+                <Link href={'/blog/' + post.id} className={classes.link}>
+                  <CardActionArea>
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {post.title}
+                        </Typography>
+                        <CardMedia
+                          className={classes.media}
+                          image={post.image}
+                          title={post.title}
+                        />
+                        <Typography variant="body2" color="textSecondary" component="p">
+                          {post.description}
+                        </Typography>
+                      </CardContent>
+                  </CardActionArea>
+                </Link>
                 <CardActions disableSpacing className={classes.cardActions}>
                   <IconButton aria-label="add to favorites"><Favorite /></IconButton>
                   <IconButton aria-label="share"><Share /></IconButton>
