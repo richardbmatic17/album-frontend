915275d4fafe9dbd3d21a890db20b0ab2d495f58
Richard Matic <richardbmatic17@gmail.com>
2021-07-27 23:46:01 +0800

    refactor blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -66,73 +66,73 @@ const useStyles = makeStyles((theme) => ({
   },
   link: {
     textDecoration: 'none !important',
+  },
+  gridList: {
+    '& .MuiGridListTile-tile': {
+      borderRadius: '10px'
+    }
   }
 }));
 
-const posts = [
+const tileData = [
   {
     id: '1',
     title: 'Boracay Adventure',
     description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
-    image: 'https://album-2021-03-02.s3-ap-southeast-1.amazonaws.com/boracay/IMG_0623.JPEG'
+    image: 'https://album-2021-03-02.s3-ap-southeast-1.amazonaws.com/boracay/IMG_0623.JPEG',
+    cols: 2
   },
   {
     id: '2',
     title: 'Random Post 1',
     description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
-    image: 'https://source.unsplash.com/random'
+    image: 'https://source.unsplash.com/random',
+    cols: 1
   },
   {
     id: '3',
     title: 'Random Post 2',
     description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
-    image: 'https://source.unsplash.com/random'
+    image: 'https://source.unsplash.com/random',
+    cols: 1
   },
   {
     id: '4',
     title: 'Random Post 3',
     description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
-    image: 'https://source.unsplash.com/random'
+    image: 'https://source.unsplash.com/random',
+    cols: 2
+  },
+  {
+    id: '5',
+    title: 'Random Post 3',
+    description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
+    image: 'https://source.unsplash.com/random',
+    cols: 1
+  },
+  {
+    id: '6',
+    title: 'Random Post 3',
+    description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
+    image: 'https://source.unsplash.com/random',
+    cols: 1
+  },
+  {
+    id: '7',
+    title: 'Random Post 3',
+    description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
+    image: 'https://source.unsplash.com/random',
+    cols: 1
+  },
+  {
+    id: '8',
+    title: 'Random Post 3',
+    description: 'This is our first time in Boracay and first time riding a plane. The experience is so awesome, we\'ll definitely try this again.',
+    image: 'https://source.unsplash.com/random',
+    cols: 3
   },
 ];
 
-const tileData = [
-  {
-    id: 1,
-    img: 'https://album-2021-03-02.s3-ap-southeast-1.amazonaws.com/boracay/IMG_0623.JPEG',
-    title: 'Boracay Adventure',
-    author: 'author',
-    cols: 1,
-  },
-  {
-    id: 2,
-    img: 'https://source.unsplash.com/random',
-    title: 'Sinagtala Adventure',
-    author: 'author',
-    cols: 2,
-  },
-  {
-    id: 3,
-    img: 'https://source.unsplash.com/random',
-    title: 'Grand Cafe 1919',
-    author: 'author',
-    cols: 2,
-  },
-  {
-    id: 4,
-    img: 'https://source.unsplash.com/random',
-    title: 'fort santiago',
-    author: 'author',
-    cols: 1,
-  },
-  {
-    id: 5,
-    img: 'https://source.unsplash.com/random',
-    title: 'Ramen Kuroda',
-    author: 'author',
-    cols: 3,
-  },
-];
 
 const Blog = (props) => {
   const classes = useStyles();
@@ -141,38 +141,10 @@ const Blog = (props) => {
     <Page>
       <Grid container spacing={3} className={classes.root}>
         <Grid item md={9} xs={12}>
-          {posts.map(post => (
-            <Paper elevation={3} className={classes.paper} key={post.title}>
-              <Card className={classes.root}>
-                <Link href={'/blog/' + post.id} className={classes.link}>
-                  <CardActionArea>
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          {post.title}
-                        </Typography>
-                        <CardMedia
-                          className={classes.media}
-                          image={post.image}
-                          title={post.title}
-                        />
-                        <Typography variant="body2" color="textSecondary" component="p">
-                          {post.description}
-                        </Typography>
-                      </CardContent>
-                  </CardActionArea>
-                </Link>
-                <CardActions disableSpacing className={classes.cardActions}>
-                  <IconButton aria-label="add to favorites"><Favorite /></IconButton>
-                  <IconButton aria-label="share"><Share /></IconButton>
-                  <IconButton aria-label="Location"><LocationOn /></IconButton>
-                </CardActions>
-              </Card>
-            </Paper>
-          ))}
-          <GridList cellHeight={300} className={classes.gridList} cols={3}>
+          <GridList cellHeight={300} cols={3}>
             {tileData.map((tile) => (
-              <GridListTile key={tile.id} cols={tile.cols || 1} xs={100}>
-                  <img src={tile.img} alt={tile.title} />
+              <GridListTile key={tile.id} cols={tile.cols || 1} xs={100} className={classes.gridList}>
+                  <img src={tile.image} alt={tile.title} />
               </GridListTile>
             ))}
           </GridList> 
